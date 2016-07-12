0f3ffc63ba50dee12c601480a47193fc77571580
JeasonWong <350062143@qq.com>
2016-07-12 16:24:12 +0800

    first commit

---
+++ app/src/main/java/me/wangyuwei/elemeshopping/MainActivity.java
@@ -0,0 +1,30 @@
+package me.wangyuwei.elemeshopping;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import me.wangyuwei.shoppoing.ShoppingView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ShoppingView mSv1 = (ShoppingView) findViewById(R.id.sv_1);
+        mSv1.setOnShoppingClickListener(new ShoppingView.ShoppingClickListener() {
+            @Override
+            public void onAddClick(int num) {
+                Log.d("@=>", "add.......num=> " + num);
+            }
+
+            @Override
+            public void onMinusClick(int num) {
+                Log.d("@=>", "minus.......num=> " + num);
+            }
+        });
+
+    }
+}
